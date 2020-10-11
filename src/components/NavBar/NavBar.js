a8766434bc442d165c04af7cceb8c3737da76a18
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-10-11 16:53:51 -0400

    added skeleton interaction for login component, added auth service and token service

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import LiquidityLogo from '../../assets/logos/LiquidityLogo.png'
+import './NavBar.css'
+
 
 export default function NavBar() {
     return (
         <div data-collapse="small" data-animation="default" data-duration="0" role="banner" class="navbar w-nav">
-        <div class="nav-bar-container w-container">
-      <Link to="/" class="customstay w-nav-brand w--current">
-        <div class="div-block-33 customstay">
-            <img src="images/Group-202.png" loading="lazy" width="96" alt="" class="image customstay"/></div>
+        <div className="nav-bar-container w-container">
+      <Link to="/" className="customstay w-nav-brand w--current">
+        <div className="div-block-33 customstay">
+            <img src={LiquidityLogo} width="96" alt="" class="image customstay"/>
+        </div>
       </Link>
-      <Link to="/" class="nav-bar-logo-block w-inline-block w--current">
-          <img src="images/Group-202.png" loading="lazy" width="96" alt="" class="nav-bar-logo"/>
+      <Link to="/" className="nav-bar-logo-block w-inline-block w--current">
+          <img src={LiquidityLogo}  width="96" alt="" class="nav-bar-logo"/>
       </Link>
-      <nav role="navigation" class="nav-menu w-nav-menu">
+      <nav role="navigation" className="nav-menu w-nav-menu">
         <div class="nav-bar-links-container">
-      <Link to="/product" class="the-nav-link w-nav-link">
+      <Link to="/product" className="the-nav-link w-nav-link">
           Product
       </Link>
-      <Link to="/pricing" class="the-nav-link w-nav-link">
+      <Link to="/pricing" className="the-nav-link w-nav-link">
           Pricing
       </Link>
-          <div class="nav_bar_separator"></div>
-        <Link to="/login" class="the-nav-link log-in w-nav-link">Log in</Link>
-        <Link to="/signup" class="the-nav-link sign-up w-nav-link">Sign up</Link></div>
+          <div className="nav_bar_separator"></div>
+        <Link to="/login" className="the-nav-link log-in w-nav-link">Log in</Link>
+        <Link to="/signup" className="the-nav-link sign-up w-nav-link">Sign up</Link></div>
       </nav>
-      <div class="menu-button w-nav-button">
-        <div class="w-icon-nav-menu"></div>
+      <div className="menu-button w-nav-button">
+        <div className="w-icon-nav-menu"></div>
       </div>
     </div>
   </div>
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,30 +1,14 @@
 import React from 'react'
 import Footer from '../../components/Footer/Footer';
 import NavBar from '../../components/NavBar/NavBar'
+import LoginForm from '../../components//LoginForm/LoginForm';
+
 export default function LoginPage(){
     return (
     <>
     <NavBar/>
-    <div>
-        <div class="signup-signin-section-container w-container">
-        <div class="sign-up-in-container">
-            <h1 class="heading-4">Log in</h1><a href="searchs/main.html" class="twitter_button w-inline-block"><img src="images/bx_bxl-twitter_1bx_bxl-twitter.png" loading="lazy" alt="" class="image-16"/><div class="text-block-6">Continue with Twitter</div></a><a href="searchs/main.html" class="gmail-button w-inline-block"><img src="images/ant-design_google-outlined_1ant-design_google-outlined.png" loading="lazy" width="20" alt="" class="image-17"/><div class="text-block-7">Continue with Google</div></a>
-            <div class="div-block-53"></div>
-            <div class="email-input-field-button-container">
-            <div class="input_field_header">Email</div>
-            <div class="email-input-field-sign-up-sign-in filled">arodriguezlebron@gmail.com</div>
-            <div class="input_field_header">Password</div>
-            <div class="email-input-field-sign-up-sign-in">Enter your password...</div>
-            <a href="searchs/main.html" class="email_button w-inline-block">
-                <div class="text-block-7">Continue with Password</div>
-            </a>
-            </div><a href="forgot-password.html" class="forgot-password-button">Forgot password?</a></div>
-        <div class="div-block-123">
-            <div class="terms-conditions small">By clicking “Continue with Twitter/Google/Email” above, you acknowledge that you have read and understood, and agree to Liquidity&#x27;s <a href="https://pdfhost.io/v/xVCZXGnoG_Terms_of_Service_Liquiditysopdf.pdf" target="_blank" class="mini-link"><span class="text-span-7">Terms &amp; Conditions</span></a> and <a href="https://pdfhost.io/v/vD3RKzKIo_Privacy_Policy_Liquiditysopdf.pdf" target="_blank"><span class="text-span-8">Privacy Policy</span></a>.</div>
-        </div>
-        </div>
-    </div>
-      <Footer/>
+        <LoginForm/>
+    <Footer/>
     </>
     )
 }
