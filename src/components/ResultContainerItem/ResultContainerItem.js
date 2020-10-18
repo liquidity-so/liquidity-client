8a1bc057f3985927bdd450f38bcb31cbfa6858de
Miguel Lorenzo <jsmglorenzo@gmail.com>
2020-10-18 17:14:14 -0400

    pdated styling of results container

--- src/components/ResultContainerItem/ResultContainerItem.js
+++ src/components/ResultContainerItem/ResultContainerItem.js
@@ -26,26 +26,3 @@ export default function ResultContainerItem(props) {
         </a>
     )
 }
-/*
-
-     <a href="https://www.probit.com/app/exchange/BAT-BTC" target="_blank" class="div-block-112 w-inline-block">
-            <div class="w-layout-grid search-results_row">
-              <div class="div-block-109">
-                <img src="../images/image-36_1image-36.png" loading="lazy" width="20" alt="" class="image-46">
-                <div class="text-block-78">Probit</div>
-              </div>
-              <div class="div-block-110 hide-on-mobile">
-                <div class="results_text">1263</div>
-              </div>
-              <div class="div-block-110 hide-on-mobile">
-                <div class="results_text">$5,104</div>
-              </div>
-              <div class="div-block-110">
-                <div class="results_text">$0.2201</div>
-              </div>
-              <div class="div-block-110 keep">
-                <div class="results_text">$39,420</div>
-              </div>
-            </div>
-          </a>
-*/
